b9fad28e4b3db37cf6f91cb592affaced733fa37
Taeuk Noh <taeuk4958@gmail.com>
2022-07-19 08:19:04 +0900

    test 0.5

--- pipeline.py
+++ pipeline.py
@@ -1,29 +1,69 @@
 import kfp
-from kfp.components import create_component_from_func
+from kfp.components import InputPath, OutputPath, create_component_from_func
 
-from test_1 import test_1
-from test_2 import test_2
-
-import argparse
+# from test_1 import test_1
+# from test_2 import test_2
 
 
+# test_1_op = create_component_from_func(func = test_1, 
+#                                        base_image = 'hibernation4958/test_1:0.4',
+#                                        output_component_file="test_1.component.yaml")
+# test_2_op = create_component_from_func(func = test_2,
+#                                        base_image = 'hibernation4958/test_2:0.4',
+#                                        output_component_file="test_2.component.yaml")
 
-test_1_op = create_component_from_func(func = test_1, 
-                                       base_image = 'hibernation4958/test_1:0.4',
-                                       output_component_file="test_1.component.yaml")
-test_2_op = create_component_from_func(func = test_2,
-                                       base_image = 'hibernation4958/test_2:0.4',
-                                       output_component_file="test_2.component.yaml")
+
+# @kfp.dsl.pipeline(name="test Example")
+# def test_pipeline(tmp_num):
+#     _test_1_op = test_1_op(tmp_num)
+#     _test_2_op = test_2_op(_test_1_op.outputs["data_output"])
+    
+#     print(f"_test_2_op : {_test_2_op}")
+
+    
+@create_component_from_func(
+    base_image = 'hibernation4958/test_1:0.5',
+    output_component_file="test_1.component.yaml")
+def test_1(args : int, 
+           save_path : OutputPath("dict")):
+    
+    import json
+    
+    print(f"args.test_1 : {args.tmp}")
+    
+    dict_tmp = {'1' : args}
+    dict_tmp['2'] = 2
+
+    with open(save_path, "w") as f:
+        json.dump(dict_tmp, f)
+
+
+@create_component_from_func(
+    base_image = 'hibernation4958/test_2:0.5',
+    output_component_file="test_2.component.yaml")
+def test_2(args : InputPath("dict"),
+           save_path : OutputPath("dict")):  
+    import json
+    
+    with open(args, "r") as f:
+        data = json.load(f)
+    
+    print(f"args.test_2 : {data}")
+    data['3'] = 3
+    dict_tmp = data
+
+    with open(save_path, "w") as f:
+        json.dump(dict_tmp, f)
+    
+    return dict_tmp
 
 
 @kfp.dsl.pipeline(name="test Example")
 def test_pipeline(tmp_num):
-    _test_1_op = test_1_op(tmp_num)
-    _test_2_op = test_2_op(_test_1_op.outputs["data_output"])
+    _test_1_op = test_1(tmp_num)
+    _test_2_op = test_2(_test_1_op.outputs["data_output"])
     
     print(f"_test_2_op : {_test_2_op}")
 
-    
-    
 if __name__=="__main__":    
     kfp.compiler.Compiler().compile(test_pipeline(), "./test_pipeline.yaml")
