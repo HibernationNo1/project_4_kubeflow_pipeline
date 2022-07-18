64057fc2120c6a881db6ffb01b468204afdf33bc
Taeuk Noh <taeuk4958@gmail.com>
2022-07-18 19:46:14 +0900

    test 0.4

--- pipeline.py
+++ pipeline.py
@@ -1,16 +1,27 @@
 import kfp
+from kfp.components import create_component_from_func
+
 from test_1 import test_1
 from test_2 import test_2
 
 import argparse
 
 
+
+test_1_op = create_component_from_func(finc = test_1, 
+                                       base_image = 'hibernation4958/test_1:0.4',
+                                       output_component_file="test_1.component.yaml")
+test_2_op = create_component_from_func(finc = test_2,
+                                       base_image = 'hibernation4958/test_2:0.4',
+                                       output_component_file="test_2.component.yaml")
+
+
 @kfp.dsl.pipeline(name="test Example")
 def test_pipeline(tmp_num):
-    test_1_op = test_1(tmp_num)
-    result = test_2(test_1_op.outputs["data_output"])
+    _test_1_op = test_1_op(tmp_num)
+    _test_2_op = test_2_op(_test_1_op.outputs["data_output"])
     
-    print(f"result : {result}")
+    print(f"_test_2_op : {_test_2_op}")
 
     
     
