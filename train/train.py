eaa59a356ae9341e5d716c0e726999502c060d69
Taeuk Noh <taeuk4958@gmail.com>
2022-07-18 16:30:39 +0900

    test print at train.py

--- train/train.py
+++ train/train.py
@@ -1,5 +1,6 @@
 import argparse
 import os
+import glob
 
 if __name__=="__main__":
     parser = argparse.ArgumentParser()
@@ -13,6 +14,11 @@ if __name__=="__main__":
     train_dataset = os.path.join(os.getcwd(), args.data_path)
     dataset = os.path.join(train_dataset, 'train_dataset.json')
     train_image_dir_path = os.path.join(train_dataset, 'train_images')
+    
+    print(f"train_dataset : {train_dataset}")
+    file_list = glob.glob(train_dataset)
+    print(f"file_list : {file_list}")
+    
    	
     if os.path.isfile(dataset):
         print("not")
