5d8383d2e7cd181604cbe4b07cc43a0278b5fca5
Taeuk Noh <taeuk4958@gmail.com>
2022-08-11 18:01:06 +0900

    add for mode: train

--- set_config.py
+++ set_config.py
@@ -14,11 +14,14 @@ def set_config(args: dict, config_path: OutputPath("dict"), ) :
     cfg_file_path = os.path.join(pipeline_taeuk4958_config_path, args['cfg'])        
     cfg = Config.fromfile(cfg_file_path)                # cfg_file_path : must be .py format
     
-    
-    
-    
-    if args['pipeline_name'] is not None : cfg.pipeline.pipeline_name = args['pipeline_name']
-    if args['pipeline_version'] is not None : cfg.pipeline.pipeline_version = args['pipeline_version']
+    import subprocess
+    talk_1 = "echo $AWS_ACCESS_KEY_ID"
+    talk_2 = "echo $AWS_SECRET_ACCESS_KEY"
+    subprocess.call([talk_1], shell=True)
+    subprocess.call([talk_2], shell=True)
+    exit()
+    if args['p_name'] is not None : cfg.pipeline.pipeline_name = args['p_name']
+    if args['p_version'] is not None : cfg.pipeline.pipeline_version = args['p_version']
     
     if args['mode'] == "record":
         if args['proportion_val'] is not None : cfg.options.proportion_val = args['proportion_val']
@@ -27,9 +30,17 @@ def set_config(args: dict, config_path: OutputPath("dict"), ) :
         if args['client_secrets'] is not None : cfg.gs.client_secrets = args['client_secrets']
         if args['ann_bk_name'] is not None : cfg.gs.ann_bucket_name = args['ann_bk_name']
         if args['dataset_bk_name'] is not None : cfg.gs.recoded_dataset_bucket_name = args['dataset_bk_name']
-        if args['dataset_vers'] is not None : cfg.gs.recoded_dataset_version = args['dataset_vers']
-   
-
+        if args['d_version'] is not None : cfg.gs.recoded_dataset_version = args['d_version']
+    
+    elif args['mode'] == "train":
+        if args['train_json'] is not None : cfg.dataset.train_file_name = args['train_json']
+        if args['val_json'] is not None : cfg.dataset.val_file_name = args['val_json']
+        if args['validate'] : cfg.train.validate = True
+        if args['finetun'] : cfg.train.finetun = True
+        if args['model_vers'] is not None : cfg.train.model_version = args['model_vers']
+        if args['d_version_t'] is not None : cfg.dataset.dataset_version = args['d_version_t']
+    
+    
      
     cfg.dump(config_path)                               # save to config_path : {wiorkspace}/outputs/set_config/data
     
