d9456b7eecdcc899a241d0b841e55480f1acedf5
Taeuk Noh <taeuk4958@gmail.com>
2022-08-12 13:38:51 +0900

    version updata 1.2.1

--- PypI/pipeline_taeuk4958/pipeline_taeuk4958/cloud/gs.py
+++ PypI/pipeline_taeuk4958/pipeline_taeuk4958/cloud/gs.py
@@ -14,18 +14,36 @@ def set_gs_credentials(client_secrets_file_name, gs_secret):
     
     os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = client_secrets_path
     
-    
+def convert_to_linebreak(environ_str):
+    flag = False
+    private_key = ""
+    for arp in list(environ_str):
+        if arp == '\\':
+            
+            private_key+=arp
+            flag = True
+            continue
+        
+        if arp == 'n' and flag == True:
+            private_key = private_key[:-1]
+            private_key +='\n'
+            flag = False
+            continue
+        
+        private_key+=arp
+    return private_key
+        
 def get_client_secrets():
-    client_secrets_dict = {'type':os.environ['type'],
-                           'project_id': os.environ['project_id'],
-                           'private_key_id': os.environ['private_key_id'],
-                           'private_key': os.environ['private_key'],
-                           'client_email': os.environ['client_email'],
-                           'client_id': os.environ['client_id'],
-                           'auth_uri': os.environ['auth_uri'],
-                           'token_uri': os.environ['token_uri'],
-                           'auth_provider_x509_cert_url': os.environ['auth_provider_x509_cert_url'],
-                           'client_x509_cert_url': os.environ['client_x509_cert_url'],
+    client_secrets_dict = {'type':convert_to_linebreak(os.environ['type']),
+                           'project_id': convert_to_linebreak(os.environ['project_id']),
+                           'private_key_id': convert_to_linebreak(os.environ['private_key_id']),
+                           'private_key': convert_to_linebreak(os.environ['private_key']),
+                           'client_email': convert_to_linebreak(os.environ['client_email']),
+                           'client_id': convert_to_linebreak(os.environ['client_id']),
+                           'auth_uri': convert_to_linebreak(os.environ['auth_uri']),
+                           'token_uri': convert_to_linebreak(os.environ['token_uri']),
+                           'auth_provider_x509_cert_url': convert_to_linebreak(os.environ['auth_provider_x509_cert_url']),
+                           'client_x509_cert_url': convert_to_linebreak(os.environ['client_x509_cert_url']),
                             }
     
 
