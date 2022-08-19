8183f219ea0e41abb4c01ff426165ae569d8db59
Taeuk Noh <taeuk4958@gmail.com>
2022-08-19 17:10:09 +0900

    modified file name

--- pipeline_config.py
+++ pipeline_config.py
@@ -3,27 +3,31 @@ import os
 import json
 
 
-class pipeline_config():
-    # kubeflow dashboard
+class Pipeline_Config():
+    ### kubeflow dashboard
     USERNAME = "winter4958@gmail.com"                  # "user@example.com"
     PASSWORD = "project_pipeline"                          # "12341234"
     NAMESPACE = "project-pipeline-1"        # "kubeflow-user-example-com"
-    HOST =  "http://3e60-1-214-32-67.ngrok.io"     # "http://192.168.0.167:80"       
+    HOST =  "http://8e7a-1-214-32-67.ngrok.io"     # "http://192.168.0.167:80"       
 
-    # docker images
-    SETCONFIG_IMAGE = 'hibernation4958/record:0.1'
+    ### docker images, component file 
+    SETCONFIG_IMAGE = 'hibernation4958/record_dataset:0.4'
     SETCONFIG_COM_FILE = None                   # "set_config.component.yaml"
 
-    RECORD_IMAGE = 'hibernation4958/record:0.1'
-    RECORD_COM_FILE = None                  # "record.component.yaml"
+    RECORD_IMAGE = 'hibernation4958/record_dataset:0.4'
+    RECORD_COM_FILE = None                      # "record.component.yaml"
 
-    SAVE_GS_IMAGE = RECORD_IMAGE            # 'hibernation4958/for_save.0.1'
-    SAVE_GS_COM_FILE = None                 # "save_dataset.component.yaml"
+    SAVE_GS_IMAGE = RECORD_IMAGE                # 'hibernation4958/for_save.0.1'
+    SAVE_GS_COM_FILE = None                     # "save_dataset.component.yaml"
 
-    LOAD_DATA_IMAGE = "hibernation4958/train:0.2"
-    LOAD_DATA_COM_FILE = None               # 
+    LOAD_DATA_IMAGE = "hibernation4958/train:0.1"
+    LOAD_DATA_COM_FILE = None                   # "load_dataset.component.yaml"
+    
+    TRAIN_IMAGES = LOAD_DATA_IMAGE
+    TRAIN_COM_FILE = None                       # "train.component.yaml"
 
 
+    ### pipeline
     # pipeline
     PIPELINE_NAME = "project_test"
     PIPELINE_PAC = "mmdet_project.yaml"
@@ -31,14 +35,16 @@ class pipeline_config():
     
     RUN_EXIST_PIPELINE = True   # 이미 upload된 pipeline으로 run을 할 시 True  // 새로운 pipeline을 compile하고 RUN할 계획이면 False
     
+    # experiment
     EXPERIMENT_NAME = "test01"
     
+    # run
     RUN_NAME = "project"
 
 
 """
-docker build record -t hibernation4958/record:0.1
-docker push hibernation4958/record:0.1
+docker build record -t hibernation4958/record_dataset:0.4
+docker push hibernation4958/record_dataset:0.4
 
 docker build train -t hibernation4958/train:0.2
 docker push hibernation4958/train:0.2
