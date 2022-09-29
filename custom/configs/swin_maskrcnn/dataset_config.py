97b31396bf9cae4e201829906dd475f7f8044af1
Taeuk Noh <taeuk4958@gmail.com>
2022-09-29 20:24:15 +0900

    config init

---
+++ custom/configs/swin_maskrcnn/dataset_config.py
@@ -0,0 +1,70 @@
+img_scale = (1333, 800)     # (720, 1280)  height, width
+
+img_norm_cfg = dict(
+    mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
+train_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(type='LoadAnnotations', with_bbox=True, with_mask=True),
+    dict(type='Resize', img_scale=img_scale, keep_ratio=True),
+    dict(type='RandomFlip', flip_ratio=0.5),
+    dict(type='Normalize', **img_norm_cfg),
+    dict(type='Pad', size_divisor=32),
+    dict(type='DefaultFormatBundle'),
+    dict(type='Collect', keys=['img', 'gt_bboxes', 'gt_labels', 'gt_masks']),
+]
+
+val_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(
+        type='MultiScaleFlipAug',
+        img_scale=img_scale,
+        flip=False,
+        transforms=[
+            dict(type='Resize', keep_ratio=True),
+            dict(type='RandomFlip'),
+            dict(type='Normalize', **img_norm_cfg),
+            dict(type='Pad', size_divisor=32),
+            dict(type='DefaultFormatBundle'),
+            dict(type='Collect', keys=['img']),
+        ])
+]
+
+test_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(
+        type='MultiScaleFlipAug',
+        img_scale=img_scale,
+        flip=False,
+        transforms=[
+            dict(type='Resize', keep_ratio=True),
+            dict(type='RandomFlip'),
+            dict(type='Normalize', **img_norm_cfg),
+            dict(type='Pad', size_divisor=32),
+            dict(type='DefaultFormatBundle'),
+            dict(type='Collect', keys=['img']),
+        ])
+]
+
+data_root = "data"
+
+data = dict(
+    samples_per_gpu=2,  # batch_size
+    workers_per_gpu=1, 
+    train=dict(
+        data_root = data_root,
+        ann_file= 'train_dataset.json',
+        img_prefix= "",          
+        pipeline=train_pipeline),
+    val=dict(
+        data_root = data_root,
+        ann_file= 'val_dataset.json',
+        img_prefix= "",          
+        pipeline=val_pipeline),
+    test=dict(
+        data_root = data_root,
+        batch_size = 10,
+        ann_file= None,                                                          # work_dir/model_dir/dataset.json
+        img_prefix="",                 # test할 image의 dir        
+        pipeline=test_pipeline))
+evaluation = dict(metric=['bbox', 'segm'])      # choise in ['bbox', 'segm']
+
