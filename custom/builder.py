d86b293a7992c8362bb6fabe4c6da1976f48f2b6
Taeuk Noh <taeuk4958@gmail.com>
2022-10-03 21:56:23 +0900

    add model builder

--- custom/builder.py
+++ custom/builder.py
@@ -1,5 +1,19 @@
-from registry import Registry
+from registry import Registry, build_from_cfg
+from dataset import CustomDataset
 
-PIPELINES = Registry('pipeline')
+
 MODELS = Registry('model')
 BACKBONES = Registry('backbone')
+
+def build_backbone(cfg):
+    return build_from_cfg(cfg, BACKBONES)
+
+def build_model(cfg_model):
+    
+    assert cfg_model.get('backbone') is not None , 'backbone must be specified in model field'
+    build_backbone(cfg_model.backbone)
+    # return build_from_cfg(cfg_model)
+    
+
+def build_dataset(cfg):
+    return CustomDataset(**cfg)
