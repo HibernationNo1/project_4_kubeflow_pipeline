586d4cdba147d540139b13333285e114aab0ed32
Taeuk Noh <taeuk4958@gmail.com>
2022-10-05 20:04:05 +0900

    for test

--- custom/builder.py
+++ custom/builder.py
@@ -1,19 +1,17 @@
 from registry import Registry, build_from_cfg
 from dataset import CustomDataset
 
-
 MODELS = Registry('model')
 BACKBONES = Registry('backbone')
-
+    
+    
 def build_backbone(cfg):
     return build_from_cfg(cfg, BACKBONES)
 
 def build_model(cfg_model):
-    
-    assert cfg_model.get('backbone') is not None , 'backbone must be specified in model field'
-    build_backbone(cfg_model.backbone)
-    # return build_from_cfg(cfg_model)
-    
+    mask_rcnn = build_from_cfg(cfg_model, MODELS)
+    print("?!")
+    exit()
 
 def build_dataset(cfg):
     return CustomDataset(**cfg)
