9a4c96172e2f4daa62f9f18da8fb7283dc5ea3ba
Taeuk Noh <taeuk4958@gmail.com>
2022-10-12 18:35:24 +0900

    add build_runner, build_optimizer, build_dp

--- custom/builder.py
+++ custom/builder.py
@@ -1,12 +1,16 @@
-from functools import partial
 import numpy as np
 import random
+import copy
 import torch
 from torch.utils.data import DataLoader
+from torch.nn.parallel import DataParallel
+from functools import partial
 
-from utils.utils import collate
+from utils.utils import collate, auto_scale_lr
 from utils.registry import Registry, build_from_cfg
 from utils.sampler import GroupSampler
+from utils.optimizer import DefaultOptimizerConstructor
+from utils.runner import DefaultRunnerConstructor
 from dataset import CustomDataset
 
 MODELS = Registry('model')
@@ -55,7 +59,65 @@ def worker_init_fn(worker_id, num_workers, seed):
     np.random.seed(worker_seed)
     random.seed(worker_seed)
     torch.manual_seed(worker_seed)
+
+
+def build_runner(cfg: dict, default_args: dict = None):
+    runner_cfg = copy.deepcopy(cfg)
+
+    constructor_cfg = dict(runner_cfg=runner_cfg,
+                           default_args=default_args)
     
+    runner_constructor = DefaultRunnerConstructor(**constructor_cfg)
+    
+    pass
 
+def build_optimizer(model, whole_cfg, logger):
+    auto_scale_lr(whole_cfg, logger)
+    cfg = whole_cfg.optimizer
+    optimizer_cfg = copy.deepcopy(cfg)
+    paramwise_cfg = optimizer_cfg.pop('paramwise_cfg', None)
+    
+    Constructor_cfg = dict(optimizer_cfg=optimizer_cfg,
+                           paramwise_cfg=paramwise_cfg)
+    optim_constructor = DefaultOptimizerConstructor(**Constructor_cfg)
+    
+    optimizer = optim_constructor(model)
+    return optimizer
 
-def build_dp():
+def build_dp(model, device='cuda', dim=0):
+    if device == 'cuda': 
+        model = model.cuda()
+    
+    return MMDataParallel(model, dim=dim)
+    
+    
+    
+class MMDataParallel(DataParallel):
+    """The DataParallel module that supports DataContainer.
+
+    MMDataParallel has two main differences with PyTorch DataParallel:
+
+    - It supports a custom type :class:`DataContainer` which allows more
+      flexible control of input data during both GPU and CPU inference.
+    - It implement two more APIs ``train_step()`` and ``val_step()``.
+
+    .. warning::
+        MMDataParallel only supports single GPU training, if you need to
+        train with multiple GPUs, please use MMDistributedDataParallel
+        instead. If you have multiple GPUs and you just want to use
+        MMDataParallel, you can set the environment variable
+        ``CUDA_VISIBLE_DEVICES=0`` or instantiate ``MMDataParallel`` with
+        ``device_ids=[0]``.
+
+    Args:
+        module (:class:`nn.Module`): Module to be encapsulated.
+        device_ids (list[int]): Device IDS of modules to be scattered to.
+            Defaults to None when GPU is not available.
+        output_device (str | int): Device ID for output. Defaults to None.
+        dim (int): Dimension used to scatter the data. Defaults to 0.
+    """
+    def __init__(self, model, dim=0, device_ids = [0]):
+        super().__init__(model, dim=dim, device_ids = device_ids)
+        self.dim = dim
+        
+    # TODO
