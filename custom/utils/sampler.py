1a1224c542580ad3e503015b50172a1aa5a86f91
Taeuk Noh <taeuk4958@gmail.com>
2022-10-18 19:38:34 +0900

    add __iter__

--- custom/utils/sampler.py
+++ custom/utils/sampler.py
@@ -9,13 +9,45 @@ class GroupSampler(Sampler):
         self.dataset = dataset      
         self.batch_size = batch_size
         
-        # width, height비교 flag
+        # image의 width, height비교 flag
         self.flag = dataset.flag.astype(np.int64)       # [0 or 1, 0 or 1, ... 0 or 1]  0 : width > height, 1 : width < height
         self.group_sizes = np.bincount(self.flag)       # [count of 0, count of 1]
         self.num_samples = 0
-        for i, size in enumerate(self.group_sizes):
+        for size in self.group_sizes:
             self.num_samples += int(np.ceil(size / self.batch_size)) * self.batch_size
-    # TODO
+    
+    def __iter__(self):
+        indices = []        # 각 iamge의 index를 담을 list
+        
+        for i, size in enumerate(self.group_sizes):
+            # image를 width, height 기준으로 나뉘어진 group
+            if size == 0:
+                continue
+            indice = np.where(self.flag == i)[0]        
+            assert len(indice) == size
+            np.random.shuffle(indice)   # shuffle 한 번 해주고
+            num_extra = int(np.ceil(size / self.batch_size)    # batch size에 대한 나머지 숫자
+                            ) * self.batch_size - len(indice)
+            indice = np.concatenate(                                # 나머지 숫자만큼의 image를 rendom하게 뽑은 후
+                [indice, np.random.choice(indice, num_extra)])
+            indices.append(indice)                                  # batch size에 딱 맞는 len을 갖도록 append
+        
+        indices = np.concatenate(indices)
+        indices = [     # batch_size의 길이를 가진 list의 list
+            indices[i * self.batch_size:(i + 1) * self.batch_size]
+            for i in np.random.permutation(
+                range(len(indices) // self.batch_size))
+        ]
+        # len(indices) == round(number of image / batch_size, 1)
+        indices = np.concatenate(indices)
+        indices = indices.astype(np.int64).tolist()         # 다시 1 dimention의 list로 
+
+        assert len(indices) == self.num_samples
+        return iter(indices)
+            
+    
+    def __len__(self):
+        return self.num_samples
             
 
 class RandomSampler(metaclass=ABCMeta):
