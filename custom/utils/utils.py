9394b0d058ec3adccedb9e95c30e592c85fd0583
Taeuk Noh <taeuk4958@gmail.com>
2022-10-20 20:46:22 +0900

    add to_tensor

--- custom/utils/utils.py
+++ custom/utils/utils.py
@@ -9,8 +9,33 @@ from itertools import repeat
 import collections.abc
 from collections.abc import Mapping, Sequence
 import torch
+import  numpy as np
 
+def to_tensor(data):
+    """Convert objects of various python types to :obj:`torch.Tensor`.
 
+    Supported types are: :class:`numpy.ndarray`, :class:`torch.Tensor`,
+    :class:`Sequence`, :class:`int` and :class:`float`.
+
+    Args:
+        data (torch.Tensor | numpy.ndarray | Sequence | int | float): Data to
+            be converted.
+    """
+
+    if isinstance(data, torch.Tensor):
+        return data
+    elif isinstance(data, np.ndarray):
+        return torch.from_numpy(data)
+    elif isinstance(data, Sequence) and not isinstance(data, str):
+        return torch.tensor(data)
+    elif isinstance(data, int):
+        return torch.LongTensor([data])
+    elif isinstance(data, float):
+        return torch.FloatTensor([data])
+    else:
+        raise TypeError(f'type {type(data)} cannot be converted to tensor.')
+    
+    
 def get_host_info():
     """Get hostname and username.
 
