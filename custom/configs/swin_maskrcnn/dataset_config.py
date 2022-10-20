e509f6cb759e649508d0bcbe2168e16fd98576b4
Taeuk Noh <taeuk4958@gmail.com>
2022-10-20 20:46:01 +0900

    add comments

--- custom/configs/swin_maskrcnn/dataset_config.py
+++ custom/configs/swin_maskrcnn/dataset_config.py
@@ -1,15 +1,18 @@
-img_scale = (1333, 800)     # (720, 1280)  height, width
+img_scale = (1333, 800)     # resizing으로 기대하는 image의 shape (1280, 720)  width, height
 
-img_norm_cfg = dict(
-    mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
+img_norm_cfg = dict(mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
 train_pipeline = [
-    dict(type='LoadImageFromFile'),
+    dict(type='LoadImageFromFile'),                                     
     dict(type='LoadAnnotations', with_bbox=True, with_mask=True),
     dict(type='Resize', img_scale=img_scale, keep_ratio=True),
-    dict(type='RandomFlip', flip_ratio=0.5),
-    dict(type='Normalize', **img_norm_cfg),
-    dict(type='Pad', size_divisor=32),
-    dict(type='DefaultFormatBundle'),
+    dict(type='RandomFlip', flip_ratio=0.5),                # flip_ratio = [0.3, 0.5, 0.2], direction = ['horizontal', 'vertical', 'diagonal']
+                                                            # 전체 중 0.3%확률로 horizontal flip적용,
+                                                            # 전체 중 0.5%확률로 vertical flip적용,
+                                                            # 전체 중 0.2%확률로 diagonal flip적용,         
+                                                            # 0.1확률로 flip적용 안함
+    dict(type='Normalize', **img_norm_cfg),                
+    dict(type='Pad', size_divisor=32),      # width, height가 각각 size_divisor의 배수가 되는 size까지 padding         
+    dict(type='DefaultFormatBundle'),                      
     dict(type='Collect', keys=['img', 'gt_bboxes', 'gt_labels', 'gt_masks']),
 ]
 
