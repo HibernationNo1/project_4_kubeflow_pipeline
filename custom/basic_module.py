be6456eabcd8be6a37255f5a7c6c73bf70f26e86
Taeuk Noh <taeuk4958@gmail.com>
2022-10-26 19:50:31 +0900

    add ConvTranspose2d

--- custom/basic_module.py
+++ custom/basic_module.py
@@ -123,7 +123,46 @@ class ConvModule(nn.Module):    # normalization, activate layers 없이 build.  
            
         return x
     
+def obsolete_torch_version(torch_version, version_threshold):
+    print(f"version_threshold : {version_threshold}")
+    return torch_version == 'parrots' or torch_version <= version_threshold
+         
+class ConvTranspose2d(nn.ConvTranspose2d):
+    def forward(self, x):
+        if x.numel() == 0 and obsolete_torch_version(TORCH_VERSION, (1, 4)):
+            out_shape = [x.shape[0], self.out_channels]
+            for i, k, p, s, d, op in zip(x.shape[-2:], self.kernel_size,
+                                         self.padding, self.stride,
+                                         self.dilation, self.output_padding):
+                out_shape.append((i - 1) * s - 2 * p + (d * (k - 1) + 1) + op)
+            empty = NewEmptyTensorOp.apply(x, out_shape)
+            if self.training:
+                # produce dummy gradient to avoid DDP warning.
+                dummy = sum(x.view(-1)[0] for x in self.parameters()) * 0.0
+                return empty + dummy
+            else:
+                return empty
+        return super().forward(x)
+
+
     
+class Conv2d(nn.Conv2d):
+    def forward(self, x):
+        if x.numel() == 0 and obsolete_torch_version(TORCH_VERSION, (1, 4)):
+            out_shape = [x.shape[0], self.out_channels]
+            for i, k, p, s, d in zip(x.shape[-2:], self.kernel_size,
+                                     self.padding, self.stride, self.dilation):
+                o = (i + 2 * p - (d * (k - 1) + 1)) // s + 1
+                out_shape.append(o)
+            empty = NewEmptyTensorOp.apply(x, out_shape)
+            if self.training:
+                # produce dummy gradient to avoid DDP warning.
+                dummy = sum(x.view(-1)[0] for x in self.parameters()) * 0.0
+                return empty + dummy
+            else:
+                return empty
+
+        return super().forward(x)  
 
 class Linear(torch.nn.Linear):
     def forward(self, x):
