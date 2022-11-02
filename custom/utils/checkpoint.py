9e84c48026062b1713d1632478a4995ad180f32c
Taeuk Noh <taeuk4958@gmail.com>
2022-11-02 19:32:26 +0900

    delete CheckpointLoader and implfy load_checkpoint

--- custom/utils/checkpoint.py
+++ custom/utils/checkpoint.py
@@ -1,138 +1,19 @@
 import torch
 from torch.optim import Optimizer
 
+import os
 import os.path as osp
 import re
 import logging
-from typing import Optional, Union, Sequence, Callable
+from typing import Optional, Union
 from collections import OrderedDict
 
 
-class CheckpointLoader:
-    """A general checkpoint loader to manage all schemes."""
-
-    _schemes: dict = {}
-    
-    @classmethod
-    def _register_scheme(cls, prefixes, loader, force=False):
-        if isinstance(prefixes, str):
-            prefixes = [prefixes]
-        else:
-            assert isinstance(prefixes, (list, tuple))
-        for prefix in prefixes:
-            if (prefix not in cls._schemes) or force:
-                cls._schemes[prefix] = loader
-            else:
-                raise KeyError(
-                    f'{prefix} is already registered as a loader backend, '
-                    'add "force=True" if you want to override it')
-        # sort, longer prefixes take priority
-        
-        cls._schemes = OrderedDict(
-            sorted(cls._schemes.items(), key=lambda t: t[0], reverse=True))
-      
-        
-        
-    @classmethod
-    def register_scheme(cls,
-                        prefixes: Union[str, Sequence[str]],
-                        loader: Optional[Callable] = None,
-                        force: bool = False):
-        """Register a loader to CheckpointLoader.
-
-        This method can be used as a normal class method or a decorator.
-
-        Args:
-            prefixes (str or Sequence[str]):
-            The prefix of the registered loader.
-            loader (function, optional): The loader function to be registered.
-                When this method is used as a decorator, loader is None.
-                Defaults to None.
-            force (bool, optional): Whether to override the loader
-                if the prefix has already been registered. Defaults to False.
-        """
-
-        if loader is not None:
-            cls._register_scheme(prefixes, loader, force=force)
-            return
-
-        def _register(loader_cls):
-            cls._register_scheme(prefixes, loader_cls, force=force)
-            return loader_cls
-
-        return _register
-    
-    
-    @classmethod
-    def load_checkpoint(
-            cls,
-            filename: str,
-            map_location: Optional[str] = None,
-            logger: Optional[logging.Logger] = None
-    ) -> Union[dict, OrderedDict]:
-        """load checkpoint through URL scheme path.
-
-        Args:
-            filename (str): checkpoint file name with given prefix
-            map_location (str, optional): Same as :func:`torch.load`.
-                Default: None
-            logger (:mod:`logging.Logger`, optional): The logger for message.
-                Default: None
-
-        Returns:
-            dict or OrderedDict: The loaded checkpoint.
-        """
-        checkpoint_loader = cls._get_checkpoint_loader(filename)      
-        class_name = checkpoint_loader.__name__
-        
-        logger.info(f'load checkpoint from {class_name[10:]} path: {filename}')
-        return checkpoint_loader(filename, map_location)
-        
-
-
-    @classmethod
-    def _get_checkpoint_loader(cls, path):
-        """Finds a loader that supports the given path. Falls back to the local
-        loader if no other loader is found.
-
-        Args:
-            path (str): checkpoint path
-
-        Returns:
-            callable: checkpoint loader
-        """
-        for p in cls._schemes:
-            # use regular match to handle some cases that where the prefix of
-            # loader has a prefix. For example, both 's3://path' and
-            # 'open-mmlab:s3://path' should return `load_from_ceph`
-            if re.match(p, path) is not None:
-                return cls._schemes[p]
-
-@CheckpointLoader.register_scheme(prefixes='')
-def load_from_local(
-        filename: str,
-        map_location: Optional[str] = None) -> Union[dict, OrderedDict]:
-    """load checkpoint by local file path.
-
-    Args:
-        filename (str): local checkpoint file path
-        map_location (str, optional): Same as :func:`torch.load`.
-
-    Returns:
-        dict or OrderedDict: The loaded checkpoint.
-    """
-    filename = osp.expanduser(filename)
-    if not osp.isfile(filename):
-        raise FileNotFoundError(f'{filename} can not be found.')
-    checkpoint = torch.load(filename, map_location=map_location)
-    return checkpoint
-
-
-@CheckpointLoader.register_scheme(prefixes=('http://', 'https://'))
 def load_from_http(
         filename: str,
         map_location: Optional[str] = None,
-        model_dir: Optional[str] = None) -> Union[dict, OrderedDict]:
+        model_dir: Optional[str] = None,
+        logger = None) -> Union[dict, OrderedDict]:
     """load checkpoint through HTTP or HTTPS scheme path. In distributed
     setting, this function only download checkpoint at local rank 0.
 
@@ -149,58 +30,45 @@ def load_from_http(
     from torch.utils.model_zoo import load_url
     checkpoint = load_url(
         filename, model_dir=model_dir, map_location=map_location)
-    return checkpoint
-
-# TODO
-def load_checkpoint(
-        model: torch.nn.Module,
-        filename: str,
-        map_location: Optional[str] = None,
-        strict: bool = False,
-        logger: Optional[logging.Logger] = None,
-        revise_keys: list = [(r'^module\.', '')]) -> Union[dict, OrderedDict]:
-    """Load checkpoint from a file or URI.
-
-    Args:
-        model (Module): Module to load checkpoint.
-        filename (str): Accept local filepath, URL, ``torchvision://xxx``,
-            ``open-mmlab://xxx``. Please refer to ``docs/model_zoo.md`` for
-            details.
-        map_location (str): Same as :func:`torch.load`.
-        strict (bool): Whether to allow different params for the model and
-            checkpoint.
-        logger (:mod:`logging.Logger` or None): The logger for error message.
-        revise_keys (list): A list of customized keywords to modify the
-            state_dict in checkpoint. Each item is a (pattern, replacement)
-            pair of the regular expression operations. Default: strip
-            the prefix 'module.' by [(r'^module\\.', '')].
-
-    Returns:
-        dict or OrderedDict: The loaded checkpoint.
-    """
-    checkpoint = CheckpointLoader.load_checkpoint(filename, map_location, logger)
-    # OrderedDict is a subclass of dict
-    if not isinstance(checkpoint, dict):
-        raise RuntimeError(
-            f'No state_dict found in checkpoint file {filename}')
-    # get state_dict from checkpoint
-    if 'state_dict' in checkpoint:
-        state_dict = checkpoint['state_dict']
+    
+    print_ = f'load checkpoint from url. path: {filename}'
+    if logger is not None:
+        logger.info(print_)
     else:
-        state_dict = checkpoint
-
-    # strip prefix of state_dict
-    metadata = getattr(state_dict, '_metadata', OrderedDict())
-    for p, r in revise_keys:
-        state_dict = OrderedDict(
-            {re.sub(p, r, k): v
-             for k, v in state_dict.items()})
-    # Keep metadata in state_dict
-    state_dict._metadata = metadata
-
-    # load state_dict
-    load_state_dict(model, state_dict, strict, logger)
+        print(print_)
+        
     return checkpoint
+
+
+def load_from_local(file_path: str, map_location='cpu', logger = None):
+    filename = osp.expanduser(file_path)
+    if not osp.isfile(filename):
+        raise FileNotFoundError(f'{filename} can not be found.')
+
+    checkpoint = torch.load(filename, map_location=map_location)
+    print_ = f'load checkpoint from local. path: {file_path}'
+    if logger is not None:
+        logger.info(print_)
+    else:
+        print(print_)
+
+    return checkpoint
+    
+    
+prefixes = {'local': load_from_local,
+            'http://': load_from_http,
+            'https://': load_from_http }
+
+def load_checkpoint(path: str, map_location='cpu', logger = None):
+    for p, func in prefixes.items():
+        if p == 'local':
+            p = osp.basename(os.getcwd())
+        if len(re.findall(p, path))==1:
+            checkpoint = func(path, map_location= map_location, logger = logger)
+  
+    return checkpoint 
+
+
 
 
 def save_checkpoint(model: torch.nn.Module,
@@ -235,11 +103,12 @@ def save_checkpoint(model: torch.nn.Module,
         meta.update(CLASSES=model.CLASSES)
     
     # create dict that with parameters of model
+    
     checkpoint = {
         'meta': meta,
         'state_dict': weights_to_cpu(get_state_dict(model))
     }
- 
+
     # save optimizer state dict in the checkpoint
     if isinstance(optimizer, Optimizer):
         checkpoint['optimizer'] = optimizer.state_dict()
@@ -335,4 +204,8 @@ def _save_to_state_dict(module: torch.nn.Module, destination: dict,
         if buf is not None:
             destination[prefix + name] = buf if keep_vars else buf.detach()
     
+
+
+
+
     
