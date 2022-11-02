6fabd8a4c4e2dfa7b99ffe0824bd1440351d76a0
Taeuk Noh <taeuk4958@gmail.com>
2022-11-02 19:28:23 +0900

    add mode: test

--- custom/train.py
+++ custom/train.py
@@ -2,6 +2,8 @@ import argparse
 import os, os.path as osp
 import numpy as np
 import torch
+import glob
+from tqdm import tqdm
 
 from utils.utils import get_device, set_meta
 from utils.config import Config
@@ -11,20 +13,22 @@ from builder import (build_model,
                      build_dataloader, 
                      build_dp, 
                      build_optimizer, 
-                     build_runner)
+                     build_runner,
+                     build_detector)
 import __init__ # to import all module and function 
 
 
-# python train.py --cfg configs/swin_maskrcnn.py --epo 3 --val_iter 50
-# python train.py --cfg configs/test.py --model_path 
+# python train.py --cfg configs/swin_maskrcnn.py --epo 50 --val_iter 50
+# python train.py --cfg configs/test.py --model_path model/model_2.pth
 
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("--cfg", required = True, help="name of config file")
     parser.add_argument('--epo', type= int, help= "max epoch in train mode")
-    parser.add_argument('--val_iter', type= int, help= "iters that run validation")
+    parser.add_argument('--val_iter', type= int, help= "iters that run validation. if -1: run val after every epoch")
     
     parser.add_argument('--model_path', type = str, help= "path of model(.pth format)") 
+    parser.add_argument('--test', action='store_true', default=False, help= "if True: run only test mode") 
     
     args = parser.parse_args()
     
@@ -38,10 +42,14 @@ def set_config(args):
     cfg.seed = np.random.randint(2**31)
     cfg.device = get_device()    
     
-    if cfg.workflow[0][0] == 'test':
+    if cfg.workflow[0][0] == 'test' or args.test:
+        cfg.workflow = [("test", None)]
+        assert cfg.model_path is not None and args.model_path is not None, f"model path is not set!"
         if args.model_path is not None: cfg.model_path = args.model_path
-        assert os.path.isfile(cfg.model_path), f"model path: '{cfg.model_path}' is not exist!"
-        
+        assert osp.isfile(cfg.model_path), f"model path: '{cfg.model_path}' is not exist!"
+        cfg.model_path = osp.join(os.getcwd(), cfg.model_path)
+        assert isinstance(cfg.data.test.batch_size, int)
+        assert osp.isdir(cfg.data.test.data_root)
         
                 
     
@@ -64,12 +72,10 @@ def set_config(args):
     
 if __name__ == "__main__":
     print(f'pytorch version: {torch.__version__}')
-    print(f'Cuda is available: {torch.cuda.is_available()}')
+    assert torch.cuda.is_available(), f'torch.cuda.is_available() is {torch.cuda.is_available()}!'
  
-
     args = parse_args()
     cfg = set_config(args)
-    
     
     logger_timestamp = set_logger_info(cfg)
     logger = create_logger('enviroments')
@@ -80,54 +86,40 @@ if __name__ == "__main__":
     dash_line = '-' * 60 + '\n'
     # logger.info('Environment info:\n' + dash_line + env_info + '\n' + dash_line)           
     # logger.info(f'Config:\n{cfg.pretty_text}')
+ 
     
-    
-    
-    # TODO: validation dataset을 build하고 validation dataloader도 build하기
-    # val_dataset = build_dataset(cfg.data.val)
-    data_loaders = []
-    train_dataset = build_dataset(cfg.data.train)
-    if cfg.model.type == 'MaskRCNN':
-        cfg.model.roi_head.bbox_head.num_classes = len(train_dataset.CLASSES) 
-        cfg.model.roi_head.mask_head.num_classes = len(train_dataset.CLASSES)    
-        # cfg.model.rpn_head.num_classes = len(train_dataset.CLASSES)   # TODO: 이거 추가 안되어있는데, 추가하고 학습에 차이있는지 확인
-    
-    train_loader_cfg = dict(
-        batch_size=cfg.data.samples_per_gpu,
-        num_workers=cfg.data.workers_per_gpu,
-        seed = cfg.seed,
-        shuffle = True)
-    data_loaders.append(build_dataloader(train_dataset, **train_loader_cfg))   
-    
-    
-
-    assert cfg.get('train_cfg') is None , 'train_cfg must be specified in both outer field and model field'
-    model = build_model(cfg.model)
-    
-    if cfg.pretrained is not None:
-        model.init_weights()
-    
-
-    
-        
-    
-    # train_detector ---
-    logger = create_logger('train')
-
-
-    model = build_dp(model, cfg.device)
-
-    
-    # build optimizer
-    optimizer = build_optimizer(model, cfg, logger)     # TODO 어떤 layer에 optimizer를 적용시켰는지 확인
-    
-    runner_meta = set_meta(cfg, args, env_info)
+    runner_meta = set_meta(cfg, args, env_info)    
     
     # flow를 통해 train, evluate를 명령어 하나헤 한 번에 실행할 수 있게. (validate은 training도중 실행) 
-    assert len(cfg.workflow) == len(data_loaders)
-    for flow, data_loader in zip(cfg.workflow, data_loaders):   # TODO : epoch단위로할지 iter단위로할지 구성    해당config확인
+    for flow in cfg.workflow:   
         mode, epoch = flow
         if mode == "train":
+            logger = create_logger('train')
+            
+            # build dataloader
+            dataset = build_dataset(cfg.data.train)
+            if cfg.model.type == 'MaskRCNN':
+                cfg.model.roi_head.bbox_head.num_classes = len(dataset.CLASSES) 
+                cfg.model.roi_head.mask_head.num_classes = len(dataset.CLASSES)    
+                # cfg.model.rpn_head.num_classes = len(train_dataset.CLASSES)   # TODO: 이거 추가 안되어있는데, 추가하고 학습에 차이있는지 확인
+            
+            train_loader_cfg = dict(batch_size=cfg.data.samples_per_gpu,
+                                    num_workers=cfg.data.workers_per_gpu,
+                                    seed = cfg.seed,
+                                    shuffle = True)
+            data_loader = build_dataloader(dataset, **train_loader_cfg)
+    
+            
+            # build model
+            assert cfg.get('train_cfg') is None , 'train_cfg must be specified in both outer field and model field'
+            model = build_model(cfg.model)
+            if cfg.pretrained is not None: model.init_weights()
+            model = build_dp(model, cfg.device)
+            
+            # build optimizer
+            optimizer = build_optimizer(model, cfg, logger)     # TODO 어떤 layer에 optimizer를 적용시켰는지 확인
+            
+            # build runner
             runner_build_cfg = dict(
                     model=model,
                     optimizer=optimizer,
@@ -136,7 +128,6 @@ if __name__ == "__main__":
                     meta=runner_meta,
                     batch_size = cfg.data.samples_per_gpu,
                     max_epochs = epoch)
-            
             train_runner = build_runner(runner_build_cfg)
 
             train_runner.timestamp = logger_timestamp
@@ -157,8 +148,10 @@ if __name__ == "__main__":
                 
                 # set CLASSES
                 cfg.checkpoint_config.meta = dict(
-                    CLASSES=train_dataset.CLASSES)
+                    CLASSES=dataset.CLASSES)
                 
+                cfg.checkpoint_config.model_cfg = cfg.model
+
             # register hooks
             cfg.log_config.iter_per_epoch = len(data_loader)
             train_runner.register_training_hooks(
@@ -180,9 +173,37 @@ if __name__ == "__main__":
             train_runner.run(data_loader, flow)
         
         elif mode == "test":
-            pass
+            test_logger = create_logger('test')
+            batch_size = cfg.data.test.batch_size
+            batch_imgs_path = glob.glob(os.path.join(cfg.data.test.data_root, "*.jpg"))
+            batch_imgs_list = [batch_imgs_path[x:x + batch_size] for x in range(0, len(batch_imgs_path), batch_size)]
+            
+            model = build_detector(cfg, cfg.model_path, device = cfg.device, logger = test_logger)
+
+            classes = model.CLASSES
+            codel_config = model.cfg  
+            model = build_dp(model, cfg.device)
+            model.cfg = codel_config  
+            outputs = []  
+            
+            from inference import inference_detector
+            for batch_imgs in tqdm(batch_imgs_list):   
+                with torch.no_grad():
+                    
+                    results = inference_detector(model, batch_imgs)
+                
+                out_files = []
+                for img_path in batch_imgs_path:
+                    file_name = os.path.basename(img_path)
+                    out_file = os.path.join(log_info['result_dir'], file_name)
+                    out_files.append(out_file)
+                
+                
+                
         
         elif mode == "val":
+            # TODO: validation dataset을 build하고 validation dataloader도 build하기
+            # val_dataset = build_dataset(cfg.data.val)
             pass
                 # TODO : validate 수행
                 # if validate:      
