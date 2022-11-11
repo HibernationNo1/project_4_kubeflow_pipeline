ddc59114a9239bed717e96740581d27b19f9ec53
Taeuk Noh <taeuk4958@gmail.com>
2022-11-11 18:09:04 +0900

    add mAP code

--- custom/train.py
+++ custom/train.py
@@ -16,7 +16,8 @@ from builder import (build_model,
                      build_optimizer, 
                      build_runner,
                      build_detector)
-from eval import inference_detector, parse_inferece_result, compute_iou, get_divided_polygon
+from eval import get_precision_recall_value
+from inference import inference_detector
 from visualization import show_result, mask_to_polygon
 
 import __init__ # to import all module and function 
@@ -264,108 +265,165 @@ if __name__ == "__main__":
                                     shuffle = True)
             _, val_dataloader = build_dataloader(**val_loader_cfg)
             
-            for i, val_data_batch in enumerate(val_dataloader):
-                gt_bboxes_list = val_data_batch['gt_bboxes'].data
-                gt_labels_list = val_data_batch['gt_labels'].data
-                img_list = val_data_batch['img'].data
-                gt_masks_list = val_data_batch['gt_masks'].data
-                assert len(gt_bboxes_list) == 1 and (len(gt_bboxes_list) ==
-                                                        len(gt_labels_list) ==
-                                                        len(img_list) == 
-                                                        len(gt_masks_list))
-                # len: batch_size
-                batch_gt_bboxes = gt_bboxes_list[0]           
-                batch_gt_labels = gt_labels_list[0]  
-                batch_gt_masks = gt_masks_list[0]    
+            model_for_val = build_detector(cfg, cfg.model_path, device = cfg.device, logger = logger)    
+            
+            precision_recall_dict = get_precision_recall_value(model_for_val, cfg, val_dataloader, mask_to_polygon)
+            
+            
+            # def compute_PR_area()         
+                                
+            
+        
+            import matplotlib.pyplot as plt
+            
+            for class_name, threshold_list in precision_recall_dict.items():
+                print(f"\n---class_name : {class_name}")
+                 
+                sorted_pr_value = []
                 
-                img_metas = val_data_batch['img_metas'].data[0]
-                batch_images_path = []    
-                for img_meta in img_metas:
-                    batch_images_path.append(img_meta['filename'])
-                    
-                model_for_val = build_detector(cfg, cfg.model_path, device = cfg.device, logger = logger)                              
-                batch_results = inference_detector(model_for_val, batch_images_path, cfg.data.val.batch_size)
+                prcs_rcl_list = []
+                for idx, threshold in enumerate(threshold_list):
+                    prcs_rcl_list.append([threshold['precision'], threshold['recall']])
+                prcs_rcl_list.reverse()
                 
-                classes = model_for_val.CLASSES
-                class_dict = dict()
-                for class_name in classes:
-                    class_dict[class_name] = dict(num_gt = 0,
-                                                  num_pred = 0,
-                                                  num_ture = 0)
+                # for idx, prcs_rcl in enumerate(prcs_rcl_list):
+                #     precision, recall = prcs_rcl
+                #     print(f"recall, precision ; {recall, precision}")
+                ap_area = 0
+                before_recall =0
+                continue_count = -1
+                for idx, prcs_rcl in enumerate(prcs_rcl_list):
+                    if continue_count > 0: 
+                        continue_count -=1 
+                        if continue_count == 0 : continue_count = -1
+                        continue
+                    precision, recall = prcs_rcl
+                    
+                    if idx+1 < len(prcs_rcl_list) and prcs_rcl_list[idx+1][1] == recall:
+                        tmp_precision_list = []
+                        for i in range(idx, len(prcs_rcl_list)):
+                            if recall == prcs_rcl_list[i][1]:       # same recall
+                                continue_count +=1
+                                tmp_precision_list.append(prcs_rcl_list[i][0])
+                            else: break
+                        precision = max([precis for precis in tmp_precision_list])
+                    
+                    print(f"before_recall : {before_recall:.2f}, recall : {recall:.2f}      ::: {abs(before_recall - recall):.2f}, {precision:.2f}     area = {abs(before_recall - recall)*precision:.2f}")
+                    area = abs(before_recall - recall)*precision
+                    ap_area += area
+                    print(f"ap_area : {ap_area}")
+                    
+                    before_recall = recall
                 
-                assert (len(batch_gt_bboxes) == 
-                            len(batch_gt_labels) ==
-                            len(batch_images_path) ==
-                            len(batch_gt_masks) ==
-                            len(batch_results))
-                   
-                for gt_mask, gt_bboxes, gt_labels, result, img_path in zip(
-                    batch_gt_masks, batch_gt_bboxes, batch_gt_labels, batch_results, batch_images_path
-                    ):
-                    i_bboxes, i_labels, i_mask = parse_inferece_result(result)
-                    img = cv2.imread(img_path)
-                    if cfg.show_score_thr > 0:
-                        assert i_bboxes is not None and i_bboxes.shape[1] == 5
-                        scores = i_bboxes[:, -1]
-                        inds = scores > cfg.show_score_thr
-                        i_bboxes = i_bboxes[inds, :]
-                        i_labels = i_labels[inds]
-                        if i_mask is not None:
-                            i_mask = i_mask[inds, ...]
+                if ap_area > 1: exit()
+                print(f"sum_ap_area : {ap_area}")  
+                # TODO : ap_area가 1보다 큰 경우는 우쨰?
+            exit()
                     
-                    i_cores = i_bboxes[:, -1]      # [num_instance]
-                    i_bboxes = i_bboxes[:, :4]      # [num_instance, [x_min, y_min, x_max, y_max]]
-
+                # before_recall = None
+                # curve_flag = False
+                # max_recall = -1
+                # prec_recall_list = []
+                # tmp_precision_list, tmp_recall_list = [], []
+                # for idx, threshold in enumerate(threshold_list):
+                #     recall, precision = threshold['recall'], threshold['precision']
+                #     prec_recall_list.append([precision, recall])
+                #     tmp_precision_list.append(precision)
+                #     tmp_recall_list.append(recall)
+                        
                     
-                    i_polygons = mask_to_polygon(i_mask)
-                    gt_polygons = mask_to_polygon(gt_mask.masks)
+                #     if before_recall is None:
+                #         before_recall = recall
+                #         continue
                     
-                    infer_dict = dict(bboxes = i_bboxes,
-                                      polygons = i_polygons,
-                                      labels = i_labels,
-                                      score = i_cores)
-                    gt_dict = dict(bboxes = gt_bboxes,
-                                   polygons = gt_polygons,
-                                   labels = gt_labels)
+                #     if before_recall - recall < 0:
+                #         curve_flag = True
+                        
+                #     if max_recall < recall : max_recall = recall
                     
-                    instance_dict = {}
-                    match_idx = []
-                    num_pred = len(infer_dict['bboxes'])                    
-                    num_gt = len(gt_dict['bboxes'])
-                    for i in range(num_pred):
-                        class_dict[classes[infer_dict['labels'][i]]]['num_pred'] +=1
-                        for j in range(num_gt):
-                            if i == 0:
-                                class_dict[classes[gt_dict['labels'][i]]]['num_pred'] +=1
-                            i_bboxes, gt_bboxes = infer_dict['bboxes'][i], gt_dict['bboxes'][j]
-                            iou = compute_iou(i_bboxes, gt_bboxes)
+                    
+                #     before_recall = recall
+                
+            
+                        
+                # ap_area = 0
+                # if curve_flag:
+                #     assert max_recall != -1
+                    
+                # else:
+                    # prec_recall_list.sort(key= lambda x:x[1])
+                    
+                    # before_recall =0
+                    # continue_count = -1
+                    # for idx, prec_recall in enumerate(prec_recall_list):
+                    #     if continue_count > 0: 
+                    #         continue_count -=1 
+                    #         continue
+                    #     precision, recall = prec_recall
+                        
+                    #     if idx+1 < len(prec_recall_list) and prec_recall_list[idx+1][1] == recall:
+                    #         tmp_precision_list = []
+                    #         for i in range(idx, len(prec_recall_list)):
+                    #             if recall == prec_recall_list[i][1]:
+                    #                 continue_count +=1
+                    #                 tmp_precision_list.append(prec_recall_list[i][0])
+                    #             else: break
+                    #         precision = max([precis for precis in tmp_precision_list])
                             
-                            if (iou > cfg.iou_threshold and          
-                                infer_dict['score'][i] > cfg.confidence_threshold and
-                                infer_dict['labels'][i] == gt_dict['labels'][j]):
-                                
-                                i_polygons, gt_polygons = infer_dict['polygons'][i], gt_dict['polygons'][j]
-                                i_xsort_bbox_list, i_ysort_bbox_list = get_divided_polygon(i_polygons, 3)
-                                gt_xsort_bbox_list, gt_ysort_bbox_list = get_divided_polygon(gt_polygons, 3)
-                            
-                                for i_xsort_bbox, gt_xsort_bbox in zip(i_xsort_bbox_list, gt_xsort_bbox_list):
-                                    if compute_iou(i_xsort_bbox, gt_xsort_bbox) < cfg.iou_threshold:  continue
-                                for i_ysort_bbox, gt_ysort_bbox in zip(i_ysort_bbox_list, gt_ysort_bbox_list):
-                                    if compute_iou(i_ysort_bbox, gt_ysort_bbox) < cfg.iou_threshold:  continue
-                                
-                                class_dict[classes[infer_dict['labels'][i]]]['num_ture'] +=1
-                                
-                                
-                                match_idx.append([i, j])
-                                
-                    for class_name, class_d in class_dict.items():
-                        recall = class_d['num_true']/class_d['num_gt']
-                        precision = class_d['num_true']/class_d['num_pred']
-                        class_dict['class_name']['F1_score'] = 2*(precision*recall)/(precision+recall)
-
+                    #     area = abs(before_recall - recall)*precision
+                    #     ap_area += area
+                        
+                    #     before_recall = recall
+                    
                     
                         
+                        
+          
+                        
+                        
+                    
+                    # if len(stac_pre_rcal) > 0:
+                        
+                        
+                    
+                    # print(f"\nthreshold: {threshold['iou_threshold']}")
+                    # print(f"num_gt : {threshold['num_gt']},       num_pred : {threshold['num_pred']},       num_true : {threshold['num_true']}")
+                    # print(f"recall: {threshold['recall']:.3f},    precision: {threshold['precision']:.3f}")
+                    # print(f"F1_score: {threshold['F1_score']:.3f}")
+                   
+                    
+                    
                 
+                    
+            exit()
+                # sorted_pr_value.sort(key = lambda x: x[1])
+                # precision_list, recall_list = [], []
+                # for precision, recall in sorted_pr_value:
+                #     precision_list.append(precision)
+                #     recall_list.append(recall)
+                
+                
+                
+                # pr_area = 0
+                # before_precision, before_recall = None, None
+                # for precision, recall in sorted_pr_value:
+                #     print(f"precision, recall : {precision, recall}")
+                #     if before_precision is None:
+                #         before_precision, before_recall = precision, recall
+                #         continue
+                    
+                #     if recall == before_recall: continue
+                    
+                #     difference_recall = before_recall-recall
+                #     pr_area += (difference_recall*before_precision)
+                #     before_precision, before_recall = precision, recall                    
+
+                # plt.plot(recall_list, precision_list)
+                # # plt.scatter(recall_list, precision_list, color='darkmagenta')
+                # plt.show()
+                
+                
+            exit()
             
                 # TODO : validate 수행
                 # if validate:      
