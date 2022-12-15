2943d245461b908a16d5c7ef06f0698e73a8d096
Taeuk Noh <taeuk4958@gmail.com>
2022-12-15 20:11:44 +0900

    modify input params

--- pipeline.py
+++ pipeline.py
@@ -13,7 +13,8 @@ from utils.check_op import check_status_op, check_status
 
 from pipeline_utils import (set_config,
                             connet_client, get_experiment, run_pipeline, upload_pipeline, 
-                            kfb_print)
+                            kfb_print,
+                            CONFIGS)
 
 from kubernetes.client.models import V1EnvVar, V1EnvVarSource, V1SecretKeySelector
 from hibernation_no1.configs.utils import get_tuple_key
@@ -21,7 +22,10 @@ from hibernation_no1.configs.utils import get_tuple_key
 SECRETS = dict()
 
 @dsl.pipeline(name="hibernation_project")
-def project_pipeline(cfg : dict, cfg_flag: dict):   
+def project_pipeline(cfg_ai : dict, cfg_ai_flag: dict, 
+                     cfg_recode : dict, cfg_recode_flag: dict
+                     ):  # TODO: rename
+     
     # ev_gs_type = V1EnvVar(name ='type', value_from= V1EnvVarSource( secret_key_ref=V1SecretKeySelector( name=client_sc_name, key = 'type')))
     # ev_gs_project_id = V1EnvVar(name ='project_id', value_from= V1EnvVarSource( secret_key_ref=V1SecretKeySelector( name=client_sc_name, key = 'project_id')))
     # ev_gs_private_key_id = V1EnvVar(name ='private_key_id', value_from= V1EnvVarSource( secret_key_ref=V1SecretKeySelector( name=client_sc_name, key = 'private_key_id')))
@@ -39,85 +43,30 @@ def project_pipeline(cfg : dict, cfg_flag: dict):
         for key in secrets_cfg:
             SECRETS[secrets_cate][key] = V1EnvVar(name=key, value_from=V1EnvVarSource(secret_key_ref=V1SecretKeySelector(name=client_sc_name, key=key)))
     
-    
-    _check_status_op = check_status_op(cfg, cfg_flag)\
-        .add_env_variable(SECRETS['gs']["type"]) \
-        .add_env_variable(SECRETS['gs']["project_id"]) \
-        .add_env_variable(SECRETS['gs']["private_key_id"]) \
-        .add_env_variable(SECRETS['gs']["private_key"]) \
-        .add_env_variable(SECRETS['gs']["client_email"]) \
-        .add_env_variable(SECRETS['gs']["client_id"]) \
-        .add_env_variable(SECRETS['gs']["auth_uri"]) \
-        .add_env_variable(SECRETS['gs']["token_uri"]) \
-        .add_env_variable(SECRETS['gs']["auth_provider_x509_cert_url"]) \
-        .add_env_variable(SECRETS['gs']["client_x509_cert_url"]) \
-        .add_env_variable(SECRETS['db']["password"]) \
-        .add_env_variable(SECRETS['db']["host"]) \
-        .add_env_variable(SECRETS['db']["port"]) \
-            
-    # _status_check_op = check_status_op(cfg, cfg_flag)\
-    #                .add_env_variable(ev_gs_type) \
-    #         .add_env_variable(ev_gs_project_id) \
-    #         .add_env_variable(ev_gs_private_key_id) \
-    #         .add_env_variable(ev_gs_private_key) \
-    #         .add_env_variable(ev_gs_client_email) \
-    #         .add_env_variable(ev_gs_client_id) \
-    #         .add_env_variable(ev_gs_auth_uri) \
-    #         .add_env_variable(ev_gs_token_uri) \
-    #         .add_env_variable(ev_gs_auth_provider_x509_cert_url) \
-    #         .add_env_variable(ev_gs_client_x509_cert_url) 
+    with dsl.Condition(cfg_recode_flag != None) :   # must be `_flag`
+        _check_status_op = check_status_op(cfg_recode, cfg_recode_flag)\
+            .add_env_variable(SECRETS['gs']["type"]) \
+            .add_env_variable(SECRETS['gs']["project_id"]) \
+            .add_env_variable(SECRETS['gs']["private_key_id"]) \
+            .add_env_variable(SECRETS['gs']["private_key"]) \
+            .add_env_variable(SECRETS['gs']["client_email"]) \
+            .add_env_variable(SECRETS['gs']["client_id"]) \
+            .add_env_variable(SECRETS['gs']["auth_uri"]) \
+            .add_env_variable(SECRETS['gs']["token_uri"]) \
+            .add_env_variable(SECRETS['gs']["auth_provider_x509_cert_url"]) \
+            .add_env_variable(SECRETS['gs']["client_x509_cert_url"]) \
+            .add_env_variable(SECRETS['db']["password"]) \
+            .add_env_variable(SECRETS['db']["host"]) \
+            .add_env_variable(SECRETS['db']["port"]) \
+        
   
-        
-    
-                      
-    # with dsl.Condition(input_mode == "record") : 	    
-    #     _record_op = record_op(cfg).after(_status_check_op) \
-    #         .add_env_variable(ev_gs_type) \
-    #         .add_env_variable(ev_gs_project_id) \
-    #         .add_env_variable(ev_gs_private_key_id) \
-    #         .add_env_variable(ev_gs_private_key) \
-    #         .add_env_variable(ev_gs_client_email) \
-    #         .add_env_variable(ev_gs_client_id) \
-    #         .add_env_variable(ev_gs_auth_uri) \
-    #         .add_env_variable(ev_gs_token_uri) \
-    #         .add_env_variable(ev_gs_auth_provider_x509_cert_url) \
-    #         .add_env_variable(ev_gs_client_x509_cert_url) 
- 
-            
-    #     _save_dataset_op = save_dataset_op(cfg, _record_op.outputs['train_dataset'], _record_op.outputs['val_dataset']) \
-    #         .add_env_variable(ev_gs_type) \
-    #         .add_env_variable(ev_gs_project_id) \
-    #         .add_env_variable(ev_gs_private_key_id) \
-    #         .add_env_variable(ev_gs_private_key) \
-    #         .add_env_variable(ev_gs_client_email) \
-    #         .add_env_variable(ev_gs_client_id) \
-    #         .add_env_variable(ev_gs_auth_uri) \
-    #         .add_env_variable(ev_gs_token_uri) \
-    #         .add_env_variable(ev_gs_auth_provider_x509_cert_url) \
-    #         .add_env_variable(ev_gs_client_x509_cert_url) 
-
-        
-    # with dsl.Condition(input_mode == "train") :
-    #     _train_op = train_op(cfg).after(_status_check_op) \
-    #         .add_env_variable(ev_gs_type) \
-    #         .add_env_variable(ev_gs_project_id) \
-    #         .add_env_variable(ev_gs_private_key_id) \
-    #         .add_env_variable(ev_gs_private_key) \
-    #         .add_env_variable(ev_gs_client_email) \
-    #         .add_env_variable(ev_gs_client_id) \
-    #         .add_env_variable(ev_gs_auth_uri) \
-    #         .add_env_variable(ev_gs_token_uri) \
-    #         .add_env_variable(ev_gs_auth_provider_x509_cert_url) \
-    #         .add_env_variable(ev_gs_client_x509_cert_url) 
-        
-        
-    #     pass
 
          
-def parse_args():
+def _parse_args():
     parser = argparse.ArgumentParser()    
     parser.add_argument("--cfg_pipeline", required = True, help="name of config file which for pipeline")       
-    parser.add_argument("--cfg_ai", required = True, help="name of config file which for ai")                           # TODO: 이름 바꾸기
+    parser.add_argument("--cfg_ai", help="name of config file which for ai")                           # TODO: rename
+    parser.add_argument("--cfg_recode", help="name of config file which for ai") 
     
     kbf_parser = parser.add_argument_group('kubeflow')
     kbf_parser.add_argument("--pipeline_pw", type = str, required = True, help="password of kubeflow dashboard")        # required
@@ -144,31 +93,51 @@ def parse_args():
     swin_parser.add_argument('--drop_path_rate', type = float, help= "drop_path_rate of SwinTransformer") 
     swin_parser.add_argument('--attn_drop_rate', type = float, help= "attn_drop_rate of SwinTransformer.SwinBlockSequence.ShiftWindowMSA.WindowMSA") 
     
-    
     args = parser.parse_args()
-    cfg_pipeline, cfg_ai = set_config(args)
-    SECRETS['gs'] = dict(cfg_pipeline.secrets.gs)  
-    SECRETS['db'] = dict(cfg_pipeline.secrets.db)  
+    input_args = vars(args)     # TODO delete
     
-    input_args = vars(args)
-    
-    return args, input_args, cfg_pipeline, cfg_ai
+    return args, input_args
 
 
+def set_intput_papams():
+    """ convert type from ConfigDict to Dict
+    """
+    params = dict()
+    
+    def convert(flag):
+        for key, item in CONFIGS.items():
+            if key == "cfg_pipeline": continue
+            if item is None:
+                if flag: params[f"{key}_flag"] = False
+                else: params[f"{key}"] = False
+                continue
+            
+            if flag: params[f"{key}_flag"] = get_tuple_key(item)
+            else: params[f"{key}"] = dict(item)
+        
+    convert(True)
+    convert(False)
+
+    return params
 
 
 if __name__=="__main__":      
-    args, input_args, cfg_pipeline, cfg_ai = parse_args()  
+    args, input_args = _parse_args()  
+    set_config(args)
 
+    SECRETS['gs'] = dict(CONFIGS.get('cfg_pipeline', None).secrets.gs)  
+    SECRETS['db'] = dict(CONFIGS.get('cfg_pipeline', None).secrets.db)  
+    
+
+    cfg_pipeline = CONFIGS.get('cfg_pipeline', None)
     kfb_print("connet to kubeflow client")
     client = connet_client(cfg_pipeline.kbf.dashboard)  
-          
+    
     kfb_print("compile pipeline")             
     kfp.compiler.Compiler().compile(
         project_pipeline,
         f"./{cfg_pipeline.kbf.pipeline.pac}"
         )
-
     
     # get experiment id by create experiment or load experiment info
     kfb_print("get experiment")
@@ -177,8 +146,7 @@ if __name__=="__main__":
     # get experiment id by create pipeline or updata pipeline version
     pipeline_id = upload_pipeline(client, cfg_pipeline.kbf.pipeline)     
      
-    cfg_pipeline_flag = get_tuple_key(cfg_pipeline)
-    params = dict(cfg=dict(cfg_ai), cfg_flag = cfg_pipeline_flag)        # parameters for pipeline run  
+    params = set_intput_papams() 
     run_pipeline(client, cfg_pipeline.kbf, experiment_id, pipeline_id, params)
     
     
