f5399844400251e306faeb22898819444bc9bf74
Taeuk Noh <taeuk4958@gmail.com>
2022-12-21 19:14:28 +0900

    add start sh

---
+++ recode.sh
@@ -0,0 +1,6 @@
+python pipeline.py \
+--cfg_pipeline pipeline_config.py \
+--pipeline_v 0.15  \
+--pipeline_pw 4958  \
+--cfg_recode config/record_cfg.py  
+
---
+++ train.sh
@@ -0,0 +1,5 @@
+python pipeline.py \
+--cfg_pipeline pipeline_config.py \
+--pipeline_v 0.21  \
+--pipeline_pw 4958  \
+--cfg_train config/train_cfg.py  
