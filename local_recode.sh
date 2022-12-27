8fb397e382e152db5941b38166209831371ad4e5
HibernationNo1 <taeuk4958@gmail.com>
2022-12-27 19:04:41 +0900

    add for run local

---
+++ local_recode.sh
@@ -0,0 +1,5 @@
+python main.py \
+--cfg_pipeline config/pipeline.py \
+--pipeline_v 0.42  \
+--pipeline_pw 4958  \
+--cfg_recode config/record_cfg.py  
---
+++ local_train.sh
@@ -0,0 +1,3 @@
+python main.py \
+--cfg_train config/train_cfg.py \
+--model MaskRCNN
