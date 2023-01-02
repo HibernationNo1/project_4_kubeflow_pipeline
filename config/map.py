dcbf9b08aa99023cf222e02eda0f43f012972335
HibernationNo1 <taeuk4958@gmail.com>
2023-01-02 19:19:04 +0900

    add model map config

---
+++ config/map.py
@@ -0,0 +1,13 @@
+dir = dict(
+    config = dict(
+        name = "config",
+        models = "models",
+        utils = "utils",
+        base = "base"
+    ),
+    components = dict(
+        recode = "recode",
+        trina = "train"
+    )
+    
+)
