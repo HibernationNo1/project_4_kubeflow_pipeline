f229b8477a627ccdeb14a0f596d0e1d01ada7693
HibernationNo1 <taeuk4958@gmail.com>
2023-01-06 11:39:15 +0900

    add 'if' for gt test config

--- pipeline_config.py
+++ pipeline_config.py
@@ -1,8 +1,9 @@
 import os, os.path as osp
 
-from hibernation_no1.configs.config import Config
+from docker.hibernation_no1.configs.config import Config
 
 CONFIGS = dict()     # parameters for pipeline run  
+MAP_CONFIG = "config/map.py"
 
 def set_cfg_recode(args, cfg):
     assert cfg.dvc.recode.train == cfg.recode.train_dataset
@@ -20,10 +21,12 @@ def set_cfg_pipeline(args, cfg):
 def set_cfg_train(args, cfg):
     # set config of model to training 
     assert args.model, f"Model to be trained must be specified!!\n"\
-        f"add `--model` option when entering the command."    
+        f"add `--model` option when entering the command."  
+
+    map_cfg = Config.fromfile(MAP_CONFIG)
     models_cfg_path = osp.join(os.getcwd(), 
-                              CONFIGS['pipeline'].dir.config.name, 
-                              CONFIGS['pipeline'].dir.config.models)
+                                map_cfg.dir.config.name, 
+                                map_cfg.dir.config.models)        
     
     model_cfg = Config.fromfile(osp.join(models_cfg_path, f"{args.model}.py")) 
     for key, item in cfg.model.get(args.model).items():
@@ -48,23 +51,31 @@ def set_cfg_train(args, cfg):
     # If get dataset with dvc, load the paths from the database.
     # And all paths were set by dvc config
     if cfg.get('dvc', None) is not None:
-        cfg.data.train.data_root = cfg.data.val.data_root = osp.join(cfg.dvc.category, 
-                                                                     cfg.dvc.recode.name, 
-                                                                     cfg.dvc.recode.version)
-        cfg.data.train.ann_file = cfg.dvc.recode.train
-        cfg.data.val.ann_file = cfg.dvc.recode.val
-    
+        if args.cfg_pipeline is not None:
+            cfg.data.train.data_root = cfg.data.val.data_root = osp.join(cfg.dvc.category, 
+                                                                        cfg.dvc.recode.name, 
+                                                                        cfg.dvc.recode.version)
+            cfg.data.train.ann_file = cfg.dvc.recode.train
+            cfg.data.val.ann_file = cfg.dvc.recode.val    
 
     if args.pm_dilation is not None: cfg.model.backbone.pm_dilation = args.pm_dilation
     if args.drop_rate is not None: cfg.model.backbone.drop_rate = args.drop_rate
     if args.drop_path_rate is not None: cfg.model.backbone.drop_path_rate = args.drop_path_rate
     if args.attn_drop_rate is not None: cfg.model.backbone.attn_drop_rate = args.attn_drop_rate    
 
+def set_cfg_infer(args, cfg):
+    if args.model_path is None:
+        raise KeyError(f"Path of model file must be specific to run inference, but got None..  add option '--model_path'")
+    
+    cfg.model_path = args.model_path
+    
+    pass
 
 CONFIG_SET_FUNCTION = dict(
     pipeline = set_cfg_pipeline,
     recode = set_cfg_recode,
-    train = set_cfg_train
+    train = set_cfg_train,
+    test = set_cfg_infer,
 )
 
 
@@ -76,12 +87,24 @@ def set_config(args):
     Args:
         args : argparse
     """
-
-    CONFIGS['pipeline'] = args.cfg_pipeline
+    
+    if (args.cfg_pipeline is not None) and (args.pipeline_v is not None) and (args.pipeline_pw is not None):
+        print("Run with kubeflow pipeline")
+        CONFIGS['pipeline'] = args.cfg_pipeline
+        
+    elif (args.cfg_pipeline is None) and (args.pipeline_v is None) and (args.pipeline_pw is None):
+        print(f"Run without kubeflow pipleine")
+        CONFIGS['pipeline'] = None
+    else:
+        raise ValueError(f"To run in pipeline of kubeflow, config, version and password of pipeline must be set.\n"\
+                         f"add options --cfg_pipeline, --pipeline_v, --pipeline_pw")
+           
+     
     CONFIGS['train'] = args.cfg_train
     CONFIGS['recode'] = args.cfg_recode
-    
-    for key, func in CONFIG_SET_FUNCTION.items():
+    CONFIGS['test'] = args.cfg_infer
+   
+    for key, func in CONFIG_SET_FUNCTION.items():        
         if CONFIGS[key] is not None:
             # Assign config only included in args 
             config =  Config.fromfile(CONFIGS[key])
