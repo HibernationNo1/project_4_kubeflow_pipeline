cc403f702203ef77b506c0928fab4a9fc99b2e0f
HibernationNo1 <taeuk4958@gmail.com>
2023-01-09 21:15:13 +0900

    add args for katib

--- pipeline_config.py
+++ pipeline_config.py
@@ -41,12 +41,24 @@ def set_cfg_train(args, cfg):
     if args.name_db is not None: cfg.db.db = args.name_db 
     if args.user_db is not None: cfg.db.user = args.user_db 
     
-    if args.save_model_name is not None: cfg.checkpoint_config.filename_tmpl = f"{args.save_model_name}"+"_{}.path"
-    if args.val_iter is not None: 
-        for i, custom_hook in enumerate(cfg.custom_hook_config):
-            if custom_hook.get('Validation_Hook', None) is not None:
-                 cfg.custom_hook_config[i].val = args.val_iter
-                 break
+    
+    # for katib
+    if args.lr is not None: cfg.optimizer.lr = float(args.lr)
+    if args.wd is not None: 
+        cfg.optimizer.weight_decay = float(args.wd)
+        assert 0.<cfg.optimizer.weight_decay and cfg.optimizer.weight_decay < 0.999
+
+    if cfg.model.backbone.type == "SwinTransformer":
+        if args.swin_drop_rate is not None : 
+            cfg.model.backbone.drop_rate = float(args.swin_drop_rate)
+            assert 0.<cfg.optimizer.weight_decay and cfg.optimizer.weight_decay < 0.999
+        if args.swin_window_size is not None : 
+            cfg.model.backbone.window_size = int(args.swin_window_size)
+            assert cfg.optimizer.window_size in [1, 3, 5, 7, 9, 11, 13, 15]
+        if args.swin_mlp_ratio is not None : 
+            cfg.model.backbone.mlp_ratio = int(args.swin_mlp_ratio)
+            assert cfg.optimizer.mlp_ratio in [i for i in range(10)] 
+         
     
     # If get dataset with dvc, load the paths from the database.
     # And all paths were set by dvc config
