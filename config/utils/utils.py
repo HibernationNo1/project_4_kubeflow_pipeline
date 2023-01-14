c48b0e98a52c482b9dcab59b6ce68460fb43798e
HibernationNo1 <taeuk4958@gmail.com>
2023-01-14 23:41:45 +0900

    add about path

--- config/utils/utils.py
+++ config/utils/utils.py
@@ -7,6 +7,12 @@ gs = dict(
     
 )
 
-volume_path = "/pvc"
+path = dict(
+    volume = "/pvc",                # pvc volume path
+    work_space = '/workspace',      # path if workspace in docker container
+    local_volume = '/opt/local-path-provisioner'\
+                   '/pvc-c16a15b9-962b-4636-a0b4-2fd18a1423ae_project-pipeline_hibernation-project-qffnp-pipeline-pvc-2'
+)
+
 
 git_repo = "pipeline_dataset"
