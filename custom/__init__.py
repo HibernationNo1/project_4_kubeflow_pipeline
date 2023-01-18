3dd7ef46ae4596aa5f1fee157978b817a3c69322
HibernationNo1 <taeuk4958@gmail.com>
2023-01-18 16:36:08 +0900

    delete custom

--- custom/__init__.py
+++
@@ -1,5 +0,0 @@
-import builder
-import transforms.compose
-import modules.swin_transformer.swin_transformer as swin_transformer
-import utils.registry as registry
-from modules.maskrcnn import maskrcnn
