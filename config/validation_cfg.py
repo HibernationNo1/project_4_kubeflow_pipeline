1533064efe791c79a892530dfffb5b3c5ad636d9
HibernationNo1 <taeuk4958@gmail.com>
2023-02-10 01:12:23 +0900

    set config for validation

---
+++ config/validation_cfg.py
@@ -0,0 +1,17 @@
+_base_ = [
+    'base/dataset_config.py'
+]
+
+val_result = "result/validation"
+model_path = None
+
+device = 'cuda:0'
+
+
+eval_cfg = dict(
+    iou_thrs = [0.3, 0.9],          # range of iou threshold
+    num_thrs_divi = 9,             # divide range of `iou_threshold` by number of `num_thrshd_divi` for compute mAP
+    num_window = 3,
+    batch_size = 4
+)
+
