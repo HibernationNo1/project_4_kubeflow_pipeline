a232bbfccf6ad52d291f0c27c735ce512622d744
HibernationNo1 <winter4958@gmail.com>
2023-03-01 20:03:43 +0900

    add config for plot

--- config/validation_cfg.py
+++ config/validation_cfg.py
@@ -12,6 +12,8 @@ eval_cfg = dict(
     iou_thrs = [0.3, 0.9],          # range of iou threshold
     num_thrs_divi = 9,             # divide range of `iou_threshold` by number of `num_thrshd_divi` for compute mAP
     num_window = 3,
-    batch_size = 4
+    batch_size = 4,
+    save_plot = True,
+    show_plot = False
 )
 
