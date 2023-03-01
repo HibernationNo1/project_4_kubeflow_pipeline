6ba5331f9681c87e23ff23ce8f81e6326e3a64ba
HibernationNo1 <winter4958@gmail.com>
2023-03-01 18:22:16 +0900

    add get_board_info

--- inference.py
+++ inference.py
@@ -6,10 +6,8 @@ import cv2
 from tqdm import tqdm
 import sys
 
-
-LOCAL_PACKAGE_PATH = '/opt/local-path-provisioner'\
-                      '/pvc-e242aba4-a270-4000-a361-064f7c3dc91e_project-pipeline_hibernation-project-gdcr5-pipeline-pvc-2'
-
+from pipeline_base_config import Path_cfg
+LOCAL_PACKAGE_PATH = osp.join('/opt/local-path-provisioner', Path_cfg.volume)
 sys.path.append(LOCAL_PACKAGE_PATH)    
     
 from hibernation_no1.configs.config import Config
@@ -20,6 +18,8 @@ from hibernation_no1.mmdet.visualization import draw_to_img
 from hibernation_no1.mmdet.data.dataset import build_dataset
 from hibernation_no1.mmdet.data.dataloader import build_dataloader
 from hibernation_no1.mmdet.eval import Evaluate
+
+from hibernation_no1.mmdet.get_info_algorithm import Get_info
 
 import numpy as np
 
@@ -61,7 +61,7 @@ def test(cfg):
             file_name = osp.basename(img_path)
             out_file = osp.join(os.getcwd(), cfg.test_result, file_name)
             out_files.append(out_file)
-            
+
         for img_path, out_file, result in zip(batch_imgs, out_files, results):
             img = cv2.imread(img_path)  
             
@@ -78,6 +78,15 @@ def test(cfg):
             
             
             cv2.imwrite(out_file, img)
+
+        
+        if cfg.get_board_info:      
+            get_info_instance = Get_info(results, model.CLASSES, score_thr = cfg.show_score_thr)
+            number_board_list = get_info_instance.get_board_info()
+
+
+
+
 
 
 
