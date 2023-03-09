4ab91fc625b66304983789e50d5de1e894644441
HibernationNo1 <winter4958@gmail.com>
2023-03-09 18:07:17 +0900

    init for test

---
+++ sh/for_test.sh
@@ -0,0 +1,11 @@
+python  pipeline.py \
+--cfg_pipeline config/pipeline.py \
+--pipeline_v 0.5 \
+--dashboard_pw 4958  \
+--cfg_train config/train_cfg.py \
+--model MaskRCNN \
+--cfg_record config/record_cfg.py \
+--cfg_eval config/evaluate_cfg.py \
+--cfg_test config/test_cfg.py \
+--model_path for_test/models/model_200.pth \
+--pipeline_n train
