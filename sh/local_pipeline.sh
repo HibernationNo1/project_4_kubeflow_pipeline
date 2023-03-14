828edc1544686b2fec05de3008ce4140b1021343
HibernationNo1 <winter4958@gmail.com>
2023-03-14 17:53:33 +0900

    init

---
+++ sh/local_pipeline.sh
@@ -0,0 +1,7 @@
+python  main.py \
+--cfg_record config/record_cfg.py \
+--cfg_train config/train_cfg.py \
+--model MaskRCNN \
+--cfg_eval config/evaluate_cfg.py \
+--cfg_test config/test_cfg.py \
+--model_path for_test/models/model_200.pth 
