64d4f974c3e1de0b9b8a1b49dca2af63742b32de
HibernationNo1 <winter4958@gmail.com>
2023-03-24 12:05:55 +0900

    fix pvc volume path

--- sh/katib_run.sh
+++ sh/katib_run.sh
@@ -1,3 +1,3 @@
 docker run --shm-size 8G \
-  -v /opt/local-path-provisioner/pvc-c5ecf260-b80c-4cf4-92ff-e5a7bf57aac1_pipeline_hibernation-project-pggtm-pipeline-pvc-2:/katib_volume \
+  -v /opt/local-path-provisioner/pvc-c5ecf260-b80c-4cf4-92ff-e5a7bf57aac1_pipeline_hibernation-project-pggtm-pipeline-pvc-2:/docker_volume \
   -it localhost:5000/katib:0.1
