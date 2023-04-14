8d53099f5c9666669b7af65c172d939ea0a2ef08
HibernationNo1 <winter4958@gmail.com>
2023-04-14 09:36:50 +0900

    init: to run component in pipeline

---
+++ sh/evaludate.sh
@@ -0,0 +1,8 @@
+python  pipeline.py \
+--cfg_pipeline config/pipeline.py \
+--pipeline_v 0.43 \
+--dashboard_pw 4958  \
+--cfg_eval config/evaluate_cfg.py \
+--model_path for_test/models/model_200.pth \
+--pipeline_n eval
+
